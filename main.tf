4d2a651f0e5b02b32edecdb326991b5324f57bf9
naoki-yazawa <naoki.yazawa@dena.com>
2024-01-18 07:26:40 +0900

    feat: restore provider

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-# provider "google" {
-#   credentials = var.GCP_CREDENTIALS
-#   project     = var.GCP_PROJECT_ID
-#   region      = var.GCP_PROJECT_REGION
-# }
+provider "google" {
+  credentials = var.GCP_CREDENTIALS
+  project     = var.GCP_PROJECT_ID
+  region      = var.GCP_PROJECT_REGION
+}
 
-# provider "google-beta" {
-#   credentials = var.GCP_CREDENTIALS
-#   project     = var.GCP_PROJECT_ID
-#   region      = var.GCP_PROJECT_REGION
-# }
+provider "google-beta" {
+  credentials = var.GCP_CREDENTIALS
+  project     = var.GCP_PROJECT_ID
+  region      = var.GCP_PROJECT_REGION
+}
 
 # module "modules" {
 #   source     = "./modules"
