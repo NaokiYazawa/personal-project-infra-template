283c1719777e7681b70bc47ba5ad1ae981597508
naoki-yazawa <naoki.yazawa@dena.com>
2024-02-18 17:10:55 +0900

    feat: add firebase auth

--- modules/firebase.tf
+++ modules/firebase.tf
@@ -3,3 +3,36 @@ resource "google_firebase_project" "default" {
 
   project = var.project_id
 }
+
+resource "google_project_service" "default" {
+  provider = google-beta
+  project  = var.project_id
+  for_each = toset([
+    "identitytoolkit.googleapis.com",
+  ])
+  service = each.key
+
+  disable_on_destroy = false
+}
+
+resource "google_identity_platform_config" "default" {
+  provider = google-beta
+  project  = var.project_id
+}
+
+resource "google_identity_platform_project_default_config" "default" {
+  provider = google-beta
+  project  = var.project_id
+  sign_in {
+    allow_duplicate_emails = false
+
+    email {
+      enabled           = true
+      password_required = true
+    }
+  }
+
+  depends_on = [
+    google_identity_platform_config.default
+  ]
+}
